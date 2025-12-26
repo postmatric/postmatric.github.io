af01b249169012fef572645272d586f06682246d
postmatric <256rtx@gmail.com>
2025-12-25 16:39:31 -0800

    update

---
+++ _next/static/IJ8wCLWZoYzKD5hZ-wy1r/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/postmatric//_next/:path+",
+        "destination": "/postmatric/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
