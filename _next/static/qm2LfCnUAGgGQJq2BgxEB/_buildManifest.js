2ec11fef2e71412b3de94b64140982dd0b335561
postmatric <256rtx@gmail.com>
2025-12-25 16:42:09 -0800

    test

--- _next/static/qm2LfCnUAGgGQJq2BgxEB/_buildManifest.js
+++ _next/static/qm2LfCnUAGgGQJq2BgxEB/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/postmatric//_next/:path+",
-        "destination": "/postmatric/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
